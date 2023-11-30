66af28656c55c33a60794c24343d2a9796a1fc5b
pahlavidavlatmirov <p.davlatmirov99@gmail.com>
2023-12-01 00:58:20 +0600

    Love Calculator(shared pref was added to hilt)

--- app/src/main/java/com/example/lovecalculator/ui/SecondFragment.kt
+++ app/src/main/java/com/example/lovecalculator/ui/SecondFragment.kt
@@ -7,14 +7,19 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
+import com.example.lovecalculator.Repository
 import com.example.lovecalculator.viewModel.LoveViewModel
 import com.example.lovecalculator.databinding.FragmentSecondBinding
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class SecondFragment : Fragment(){
 
     private lateinit var binding: FragmentSecondBinding
+    @Inject
+    lateinit var repository: Repository
     private val viewModel: LoveViewModel by viewModels()
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -22,14 +27,13 @@ class SecondFragment : Fragment(){
         binding = FragmentSecondBinding.inflate(inflater,container,false)
         return binding.root
     }
-
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val fname: String? = arguments?.getString("fname")
         val sname: String? = arguments?.getString("sname")
         viewModel.getLiveData(fname.toString(),sname.toString())
             .observe(viewLifecycleOwner, Observer {
-                binding.tvRes.text = it.toString()
+                binding.tvRes.text = it?.percentage
             })
     }
 
